298ec3222a2e1d23e5dd7e5cc1c5fa866825c27c
waltercoan <walter.coan@gmail.com>
2021-06-18 01:22:33 +0000

    listagem de pacientes

--- src/main/java/br/univille/walterdsi2021/controller/PacienteController.java
+++ src/main/java/br/univille/walterdsi2021/controller/PacienteController.java
@@ -1,16 +1,23 @@
 package br.univille.walterdsi2021.controller;
-
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import br.univille.walterdsi2021.model.Paciente;
+import br.univille.walterdsi2021.service.PacienteService;
 
 @Controller
 @RequestMapping("/paciente")
 public class PacienteController {
+
+    @Autowired
+    private PacienteService service;
     
     @GetMapping
     public ModelAndView index(){
-        return new ModelAndView("paciente/index");    
+        List<Paciente> listaPacientes = service.getAll();
+        return new ModelAndView("paciente/index", "listaPacientes", listaPacientes);    
     }
 }
