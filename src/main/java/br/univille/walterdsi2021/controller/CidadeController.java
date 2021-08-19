d49808ae47bd62cbd709cc1c1f003dad822d5ecb
waltercoan <walter.coan@gmail.com>
2021-08-19 22:41:12 +0000

    passo 05 - alterar uma cidade

--- src/main/java/br/univille/walterdsi2021/controller/CidadeController.java
+++ src/main/java/br/univille/walterdsi2021/controller/CidadeController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -33,5 +34,9 @@ public class CidadeController {
     public ModelAndView save(Cidade cidade){
         service.save(cidade);
         return new ModelAndView("redirect:/cidade");
+    }
+    @GetMapping("/alterar/{id}")
+    public ModelAndView alterar(@PathVariable("id") Cidade cidade){
+        return new ModelAndView("/cidade/form","cidade",cidade);
     }
 }
