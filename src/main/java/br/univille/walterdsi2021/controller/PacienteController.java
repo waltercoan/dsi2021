f29db189528a4f7a9f090d08401df89cacfa2b3a
waltercoan <walter.coan@gmail.com>
2021-08-19 22:51:43 +0000

    passo 07 - lista de cidades no cadastro de paciente

--- src/main/java/br/univille/walterdsi2021/controller/PacienteController.java
+++ src/main/java/br/univille/walterdsi2021/controller/PacienteController.java
@@ -1,4 +1,5 @@
 package br.univille.walterdsi2021.controller;
+import java.util.HashMap;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -8,7 +9,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import br.univille.walterdsi2021.model.Cidade;
 import br.univille.walterdsi2021.model.Paciente;
+import br.univille.walterdsi2021.service.CidadeService;
 import br.univille.walterdsi2021.service.PacienteService;
 
 @Controller
@@ -17,6 +21,9 @@ public class PacienteController {
 
     @Autowired
     private PacienteService service;
+
+    @Autowired
+    private CidadeService cidadeService;
     
     @GetMapping
     public ModelAndView index(){
@@ -25,7 +32,10 @@ public class PacienteController {
     }
     @GetMapping("/novo")
     public ModelAndView novo(@ModelAttribute Paciente paciente){
-        return new ModelAndView("paciente/form");
+        HashMap<String,Object> dados = new HashMap<>();
+        List<Cidade> listaCidades = cidadeService.getAll();
+        dados.put("listaCidades", listaCidades);
+        return new ModelAndView("paciente/form",dados);
     }
     @PostMapping(params="form")
     public ModelAndView save(Paciente paciente){
@@ -35,7 +45,11 @@ public class PacienteController {
 
     @GetMapping("/alterar/{id}")
     public ModelAndView alterar(@PathVariable("id") Paciente paciente){
-        return new ModelAndView("/paciente/form","paciente",paciente);
+        HashMap<String,Object> dados = new HashMap<>();
+        List<Cidade> listaCidades = cidadeService.getAll();
+        dados.put("listaCidades", listaCidades);
+        dados.put("paciente",paciente);
+        return new ModelAndView("paciente/form",dados);
     }
 
     @GetMapping("/delete/{id}")
