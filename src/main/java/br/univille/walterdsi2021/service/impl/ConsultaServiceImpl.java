ae86c2dfaee5367b39d81fbf9b7d89fa9d7ea592
waltercoan <walter.coan@gmail.com>
2021-08-26 17:56:54 +0000

    passo 1 - criação do repositorio da consulta e do serviço

---
+++ src/main/java/br/univille/walterdsi2021/service/impl/ConsultaServiceImpl.java
@@ -0,0 +1,31 @@
+package br.univille.walterdsi2021.service.impl;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import br.univille.walterdsi2021.model.Consulta;
+import br.univille.walterdsi2021.repository.ConsultaRepository;
+import br.univille.walterdsi2021.service.ConsultaService;
+
+@Service
+public class ConsultaServiceImpl implements ConsultaService {
+
+    private ConsultaRepository repository;
+
+    @Override
+    public List<Consulta> getAll() {
+        return repository.findAll();
+    }
+
+    @Override
+    public void save(Consulta consulta) {
+        repository.save(consulta);
+    }
+
+    @Override
+    public void delete(Consulta consulta) {
+        repository.delete(consulta);
+    }
+    
+}
