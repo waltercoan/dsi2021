931e8bf46e980ec9ea7b499f01c3f73682b5fcca
waltercoan <walter.coan@gmail.com>
2021-09-02 21:58:15 +0000

    passo 1 - criação da entidade e do repositório com o metodo de busca customizado

---
+++ src/main/java/br/univille/walterdsi2021/repository/PostRepository.java
@@ -0,0 +1,14 @@
+package br.univille.walterdsi2021.repository;
+
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.univille.walterdsi2021.model.Post;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post,Long>{
+    List<Post> findAllTopTenOrderByDataPostDesc();
+}
