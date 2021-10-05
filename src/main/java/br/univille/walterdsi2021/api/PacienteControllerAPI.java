88a88afe07fafa84cf7318b9833a0cbe8c416cd5
waltercoan <walter.coan@gmail.com>
2021-10-05 22:39:04 +0000

    finalizacao API REST

--- src/main/java/br/univille/walterdsi2021/api/PacienteControllerAPI.java
+++ src/main/java/br/univille/walterdsi2021/api/PacienteControllerAPI.java
@@ -5,9 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,13 +22,14 @@ public class PacienteControllerAPI {
     
     @Autowired
     private PacienteService service;
-
+    //CRUD
+    //Read
     @GetMapping
     public ResponseEntity<List<Paciente>> getAll(){
         List<Paciente> listaPaciente = service.getAll();
         return new ResponseEntity<List<Paciente>>(listaPaciente, HttpStatus.OK);
     }
-
+    //Create
     @PostMapping
     public ResponseEntity<Paciente> insert(@RequestBody Paciente paciente){
         try{
@@ -51,5 +54,24 @@ public class PacienteControllerAPI {
     public ResponseEntity<Paciente> getById(@PathVariable("id") Paciente paciente){
         return new ResponseEntity<Paciente>(paciente,HttpStatus.OK);
     }
+    //DELETE
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Paciente> delete(@PathVariable("id") Paciente paciente){
+        service.delete(paciente);
+        return new ResponseEntity<>(paciente,HttpStatus.OK);
+    }
+    //UPDATE
+    @PutMapping("/{id}")
+    public ResponseEntity<Paciente> update (@RequestBody Paciente pacienteNovo, 
+                                            @PathVariable("id") Paciente pacienteAntigo){
+        pacienteAntigo.setNome(pacienteNovo.getNome());
+        pacienteAntigo.setSexo(pacienteNovo.getSexo());
+        pacienteAntigo.setUsuario(pacienteNovo.getUsuario());
+        pacienteAntigo.setCidade(pacienteNovo.getCidade());
+        service.save(pacienteAntigo);
+        
+        return new ResponseEntity<>(pacienteAntigo,HttpStatus.OK);
+    }
+
 
 }
