fa794035cf69b23ed9cb0d754a4a598a7781c05e
waltercoan <walter.coan@gmail.com>
2021-10-26 01:33:23 +0000

    implementação da autenticação da API JWT

--- src/main/java/br/univille/walterdsi2021/security/SecurityConfigurer.java
+++ src/main/java/br/univille/walterdsi2021/security/SecurityConfigurer.java
@@ -2,27 +2,42 @@ package br.univille.walterdsi2021.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import br.univille.walterdsi2021.service.impl.MyUserDetailsServiceImpl;
 
 @EnableWebSecurity
 public class SecurityConfigurer extends WebSecurityConfigurerAdapter{
     
+    @Autowired
+    private JWTRequestFilter jwtRequestFilter;
+
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
         .authorizeRequests()
-        .antMatchers("/fonte_dados/**").permitAll()
+        .antMatchers("/fonte_dados/**","/api/v1/auth/signin").permitAll()
+        .antMatchers("/api/v1/").authenticated()
         .anyRequest().authenticated().and().formLogin();
+        //.and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+         
+        //httpSecurity.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
 
-        httpSecurity.csrf().ignoringAntMatchers("/fonte_dados/**");
+        httpSecurity.csrf().ignoringAntMatchers("/fonte_dados/**","/api/v1/auth/signin");
         httpSecurity.headers().frameOptions().sameOrigin();
+    }
+    @Override
+    @Bean
+    protected AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
     }
     @Autowired
     private MyUserDetailsServiceImpl myUserDetailService;
